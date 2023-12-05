b1d4e05abb8b5f80f89a44c48f4f4e4adb1c3e34
FloresTristan <michaeltristanflores646@gmail.com>
2023-12-05 23:27:28 +0800

    Created the Main to call the controllers

---
+++ CallControllers.java
@@ -0,0 +1,11 @@
+
+import javax.swing.*;
+import finals.exam.cite.*;
+
+
+public class CallControllers{
+	public static void main(String args[]){
+	JFrame frame = new JFrame("VEHICLE REGISTRATION");
+	new VehicleRegistrationController(frame);
+	}
+}

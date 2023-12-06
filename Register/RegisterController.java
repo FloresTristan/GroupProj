1072e5d53cbdf7ced6fc56745581136df6ce5d56
FloresTristan <michaeltristanflores646@gmail.com>
2023-12-06 21:59:30 +0800

    created the package, and structured it in MVC architecture

---
+++ Register/RegisterController.java
@@ -0,0 +1,13 @@
+package finals.exam.register;
+
+import javax.swing.*;
+
+public class RegisterController{
+	private JFrame frame;
+
+	public RegisterController(JFrame frame){
+		this.frame = frame;
+		new RegisterView(frame, this);
+
+	}
+}

f9df923d86fc67aee2c6030e8c1f2b3f4e444f6b
nacuarhynier <rhyniermolinanacua@gmail.com>
2023-12-10 18:29:15 +0800

    remove uncessary

--- admin/Vehicle.java
+++ admin/Vehicle.java
@@ -1,30 +1,3 @@
-/*package github.group.login;
-
-public class Vehicle {
-    String name;
-    String type;
-    String make;
-    int year;
-    String color;
-    String plateNo;
-    String license;
-    int roleId;
-
-    public Vehicle(String name, String make, String type, int year, String color,
-                   String plateNo, String license, int roleId) {
-        this.name = name;
-        this.make = make;
-        this.type = type;
-        this.year = year;
-        this.color = color;
-        this.plateNo = plateNo;
-        this.license = license;
-        this.roleId = roleId;
-    }
-}*/
-
-
-
 package github.group.admin;
 
 import java.util.ArrayList;
